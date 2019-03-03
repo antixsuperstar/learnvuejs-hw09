409e36baf5564f163512f5655250e3e453a3f279
Marcos García Yeh <antixsuperstar@gmail.com>
2019-03-03 14:25:38 -0600

    First alternative of solution, using watch and computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,85 @@
 <template>
     <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="row">
+            <div class="col-12 col-sm-8 offset-md-2 col-md-6 offset-md-3">
+                <form>
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
                     <!-- Full Name (First Name + Last Name) -->
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
+                    <full-name
+                            v-model="userData.fullname"></full-name>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input
+                                type="email"
+                                id="email"
+                                v-model.trim="userData.email"
+                                class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                v-model="userData.password"
+                                class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Store data?</label>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                            <input
+                                    type="radio"
+                                    value="true"
+                                    v-model="userData.storeData"
+                                    class="form-check-input">
+                            <span>Yes</span>
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                            <input
+                                    type="radio"
+                                    value="false"
+                                    v-model="userData.storeData"
+                                    class="form-check-input">
+                            <span>No</span>
+                            </label>
+                        </div>
+                    </div>
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+                    <div class="form-group">
+                        <button
+                                class="btn btn-primary"
+                                @click.prevent="isSubmitted = true">
+                            Submit
+                        </button>
+                    </div>
 
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
-                </div>
+                </form>
             </div>
-        </form>
+        </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="row" v-if="isSubmitted">
+            <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ userData.fullname }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ userData.storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +88,26 @@
 </template>
 
 <script>
+    import Fullname from "./components/Fullname.vue";
+
     export default {
+        data() {
+            return {
+                userData: {
+                    fullname: '',
+                    email: '',
+                    password: '',
+                    storeData: true
+                },
+                isSubmitted: false
+            }
+        },
+        components: {
+            fullName: Fullname
+        },
+        mounted() {
+            document.getElementById('firstname').focus();
+        }
     }
 </script>
 
---
+++ src/components/Fullname.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="form-group">
+        <div class="form-group">
+            <label for="firstname">First name</label>
+            <input
+                    type="text"
+                    id="firstname"
+                    class="form-control"
+                    v-model="firstname" />
+        </div>
+        <div class="form-group">
+            <label for="lastname">Last name</label>
+            <input
+                    type="text"
+                    id="lastname"
+                    class="form-control"
+                    v-model="lastname" />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                firstname: '',
+                lastname: ''
+            }
+        },
+        watch: {
+            firstname() {
+                this.$emit('input', this.value);
+            },
+            lastname() {
+                this.$emit('input', this.value);
+            }
+        },
+        computed: {
+            value() {
+                return this.firstname + ' ' + this.lastname;
+            }
+        }
+    }
+</script>
